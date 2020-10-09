c94b5eefec9231cd356e160e30056be122891924
kenjos75 <kenjos75@yahoo.com>
2020-10-09 18:19:59 +0800

    Wrapped the App inside the User Provider. Added login functionality.

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState } from 'react'
 import MainContainer from './containers/MainContainer'
-import { Provider } from 'react-redux'
 
 import {
   StyleSheet,
@@ -9,13 +8,20 @@ import {
   View
 } from 'react-native'
 
-//import our store
-import store from './store'
+import UserContext from './contexts/user.context';
 
 export default function App() {
+
+  //use hook to set state of the isLoggedIn
+  const [isLoggedIn, checkLogin] = useState(false)
+
+  //set default implementation of setting of login state
+  const setLogin = () => checkLogin(true)
+
+
   return (
-    <Provider store={store}>
-      <MainContainer />
-    </Provider>
+      <UserContext.Provider value={{isLoggedIn, setLogin}}>
+        <MainContainer />
+      </UserContext.Provider>
   )
 }
--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -17,47 +17,72 @@ import UserContext from '../contexts/user.context'
 
 function LoginScreen() {
 
+  //set username , password states
+  let [username, setUsername] = useState(null)
+  let [password, setPassword] = useState(null)
+  let [width, setWidth] = useState(Dimensions.get('window').width)
+  let [height, setHeight] = useState(Dimensions.get('window').height)
+
+
+  //listen on changes on layout
   function onLayout() {
 
+    //get dimension width and height
     let dimensionWidth = Dimensions.get('window').width
     let dimensionHeight = Dimensions.get('window').height
 
+
+    //set new width and height if the dimension does not match with the current set width, height
     if (dimensionWidth !== width || dimensionHeight !== height) {
         setWidth(dimensionWidth)
         setHeight(dimensionHeight)
     }
   }
 
-  let [username, setUsername] = useState(null)
-  let [password, setPassword] = useState(null)
-  let [width, setWidth] = useState(Dimensions.get('window').width)
-  let [height, setHeight] = useState(Dimensions.get('window').height)
+  //login
+  function login(callback) {
+
+    if (username!=='' && password!=='') {
+      //call the callback when the username and pasword is filled up
+      callback()
+    } else {
+      alert('Please enter username and password.')
+    }
+  }
 
   return (
-    <KeyboardAvoidingView style={styles.container} behavior={ Platform.Os== "ios" ? "padding" : "height" }>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.container} onLayout={()=>onLayout()}>
-          <View style={styles.titleWrapper}>
-            <Text style={styles.title}>Viseo</Text>
-          </View>
-          <View style={styles.inputFieldWrapper}>
-            <TextInput style={styles.inputField} value={username} onChangeText={username => {setUsername(username)}} placeholderStyle={styles.inputFieldPlaceHolder} placeholder="Enter your username" placeholderTextColor="#778EA9" />
-          </View>
-          <View style={styles.inputFieldWrapper}>
-            <TextInput style={styles.inputField} value={password} onChangeText={password => {setPassword(password)}} placeholderStyle={styles.inputFieldPlaceHolder} placeholder="Enter your password" placeholderTextColor="#778EA9" secureTextEntry={true} />
-          </View>
-          <View style={styles.buttonWrapper}>
-            <TouchableOpacity>
-              <Text>Login</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+    <UserContext.Consumer>
+      {
+        ({ setLogin }) => {
+          return (
+            <KeyboardAvoidingView style={styles.container} behavior={ Platform.Os== "ios" ? "padding" : "height" }>
+              <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.container} onLayout={()=>onLayout()}>
+                  <View style={styles.titleWrapper}>
+                    <Text style={styles.title}>Viseo</Text>
+                  </View>
+                  <View style={styles.inputFieldWrapper}>
+                    <TextInput style={styles.inputField} value={username} onChangeText={username => {setUsername(username)}} placeholderStyle={styles.inputFieldPlaceHolder} placeholder="Enter your username" placeholderTextColor="#778EA9" />
+                  </View>
+                  <View style={styles.inputFieldWrapper}>
+                    <TextInput style={styles.inputField} value={password} onChangeText={password => {setPassword(password)}} placeholderStyle={styles.inputFieldPlaceHolder} placeholder="Enter your password" placeholderTextColor="#778EA9" secureTextEntry={true} />
+                  </View>
+                  <TouchableOpacity onPress={() => {login(setLogin)}} style={styles.buttonWrapper}>
+                    <Text style={styles.buttonText}>Login</Text>
+                  </TouchableOpacity>
+                </View>
+              </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+          )
+        }
+      }
 
+    </UserContext.Consumer>
   )
 
 }
+
+//set stylesheets here
 let LoginStyleSheet = StyleSheet.create({
   container: {
     flex: 1,
@@ -80,14 +105,20 @@ let LoginStyleSheet = StyleSheet.create({
   },
   buttonWrapper: {
     width: 200,
-    height: 30,
+    height: 40,
     borderRadius: 30,
     marginTop: 30,
-    backgroundColor: 'pink'
+    backgroundColor: 'green'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    paddingTop: 10,
+    textAlign: 'center'
   },
   inputFieldWrapper: {
     width: 300,
-    height: 40,
+    height: 30,
     marginTop: 10,
     backgroundColor: 'white'
   },
