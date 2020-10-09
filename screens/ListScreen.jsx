034764d22d91ee36cb26aa801eb1a6a1984ff354
Kenneth John Rosales <kenjos75@yahoo.com>
2020-10-09 16:50:37 +0800

    Set initial List Screen.

---
+++ screens/ListScreen.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+
+import {
+  View,
+  Text
+} from 'react-native'
+
+function ListScreen(){
+  return (
+      <View>
+        <Text>List Screen</Text>
+      </View>
+  )
+}
+
+export default ListScreen
