6a618ad150dd711540701c46ec1ea20ceb94bb57
kenjos75 <kenjos75@yahoo.com>
2020-10-09 18:19:24 +0800

    Set default user context value.

--- contexts/user.context.js
+++ contexts/user.context.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 //user data
 let defaultValues = {
   isLoggedIn: false,
-  user: {}
+  setLogin: () => {}
 }
 
 //instantiate a user context with the methods values
