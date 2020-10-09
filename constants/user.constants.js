b547bb5f24ec3e459bb7298c640123fbffd2643d
kenjos75 <kenjos75@yahoo.com>
2020-10-09 23:43:00 +0800

    Deleted reducers, redux since I am using contexts for storing data.

--- constants/user.constants.js
+++
@@ -1,5 +0,0 @@
-const userConstants = {
-  SET_STATE: 'USER_SET_STATE'
-}
-
-export default userConstants
--- reducers/index.js
+++
@@ -1,8 +0,0 @@
-import authentication from './authentication.reducer'
-import { combineReducers } from 'redux'
-
-const rootReducer = combineReducers({
-	authentication
-})
-
-export default rootReducer
